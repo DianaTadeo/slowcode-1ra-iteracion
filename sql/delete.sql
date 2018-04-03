5b9487bcd7c59834a17b78fa463cc6f2d7f59328
emmanuel <emmanuelanaya@outlook.com>
2018-04-03 15:08:20 -0500

    Agrega scripts de base de datos

---
+++ sql/delete.sql
@@ -0,0 +1,9 @@
+begin;
+drop table carrera cascade;
+drop table usuario_sin_validar cascade;
+drop table usuario cascade;
+drop table administrador cascade;
+drop table categoria cascade;
+drop table pregunta cascade;
+drop table respuesta cascade;
+commit;
