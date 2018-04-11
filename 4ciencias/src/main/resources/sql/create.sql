6a9ca4d27da3153af5d20c38f6246999b7e8f61c
emmanuel <emmanuelanaya@outlook.com>
2018-04-10 19:42:19 -0500

    Proyecto basico con scripts de creacion

---
+++ 4ciencias/src/main/resources/sql/create.sql
@@ -0,0 +1,48 @@
+begin;
+create table carrera(
+       id serial primary key,
+       nombre varchar(100) unique not null
+);
+
+create table usuario_sin_validar(
+       id serial primary key,
+       nombre varchar(100) not null,
+       email varchar(100) not null unique,
+       password char(64) not null
+);
+
+create table usuario(
+       id serial primary key,
+       nombre varchar(100) not null,
+       email varchar(100) unique not null,
+       password char(64) not null,
+       fecha_registro timestamp not null,
+       id_carrera int references carrera(id)
+);
+
+create table administrador(
+       id serial primary key references usuario(id)
+);
+
+create table categoria(
+       id serial primary key,
+       nombre varchar(100) unique not null
+);
+
+create table pregunta(
+       id serial primary key,
+       titulo text not null,
+       contenido text,
+       fecha timestamp not null,
+       id_categoria integer references categoria(id),
+       id_usuario integer references usuario(id) not null
+);
+
+create table respuesta(
+       id serial primary key,
+       contenido text not null,
+       fecha timestamp not null,
+       id_usuario integer references usuario(id) not null,
+       id_pregunta integer references pregunta(id) not null
+);
+commit;
