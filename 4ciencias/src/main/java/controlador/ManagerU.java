bbfc43172b92c6d58fce64a8727b5c2115f8e3db
emmanuel <emmanuelanaya@outlook.com>
2018-04-16 11:41:29 -0500

    Agrega clases java

---
+++ 4ciencias/src/main/java/controlador/ManagerU.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controlador;
+
+import java.util.List;
+import modelo.HibernateUtil;
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+/**
+ *
+ * @author rrios
+ */
+public class ManagerU {
+     
+    public boolean exists(String nombre, String password) {
+        Session sesion = HibernateUtil.getSessionFactory().openSession();
+        Transaction tr = null;
+        String hql = "Select U.id FROM Usuario U where U.nombre = '" + nombre + 
+                     "' AND U.password = '" + password + "'";
+        List resultado = null;
+        try {
+            tr = sesion.beginTransaction();
+            resultado = sesion.createQuery(hql).list();
+            tr.commit();
+        }
+        catch(HibernateException he) {
+            if (tr != null) 
+                tr.rollback();
+        }
+        finally {
+            sesion.close();
+        }
+        return (resultado != null && !resultado.isEmpty());
+    }
+}
