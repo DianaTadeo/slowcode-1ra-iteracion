48c821c68f733ecf4c7f5a13fbf8a1e01694c84c
emmanuel <emmanuelanaya@outlook.com>
2018-04-30 15:25:47 -0500

    Modifica script de BD

--- 4ciencias/src/main/resources/sql/create.sql
+++ 4ciencias/src/main/resources/sql/create.sql
@@ -6,40 +6,51 @@ create table carrera(
 
 create table usuario_sin_validar(
        id serial primary key,
-       nombre varchar(100) unique not null,
-       email varchar(100) not null unique,
+       nombre varchar(100) unique
+       	      		   not null,
+       email varchar(100) unique
+       	     		  not null,
        password char(64) not null
 );
 
 create table usuario(
        id serial primary key,
-       nombre varchar(100) unique not null,
-       email varchar(100) unique not null,
+       nombre varchar(100) unique
+			   not null,
+       email varchar(100) unique
+			  not null,
        password char(64) not null,
-       fecha_registro timestamp not null,
-       id_carrera int references carrera(id),
-       es_admin boolean not null
+       fecha_registro timestamp not null
+       		      		default now(),
+       id_carrera int references carrera(id) on delete set null,
+       es_admin boolean not null,
+       foto bytea
 );
 
 create table categoria(
        id serial primary key,
-       nombre varchar(100) unique not null
+       nombre varchar(100) unique
+			   not null
 );
 
 create table pregunta(
        id serial primary key,
        titulo text not null,
        contenido text,
-       fecha timestamp not null,
-       id_categoria integer references categoria(id),
-       id_usuario integer references usuario(id) not null
+       fecha timestamp not null
+       	     	       default now(),
+       id_categoria integer references categoria(id) on delete set null,
+       id_usuario integer references usuario(id) on delete cascade
+       		  	  not null
 );
 
 create table respuesta(
        id serial primary key,
        contenido text not null,
-       fecha timestamp not null,
-       id_usuario integer references usuario(id) not null,
-       id_pregunta integer references pregunta(id) not null
+       fecha timestamp not null default now(),
+       id_usuario integer references usuario(id) on delete cascade
+       		  	  not null,
+       id_pregunta integer references pregunta(id) on delete cascade
+       		   	   not null
 );
 commit;
